61f64041b4068eb8fd8201ffda43bfb52bea299e
Simon Binder <oss@simonbinder.eu>
2022-10-11 10:48:09 +0200

    Update bootstrap and pico

--- tools/bin/upload_bootstrap.dart
+++ tools/bin/upload_bootstrap.dart
@@ -3,7 +3,7 @@ import 'package:git/git.dart';
 import 'package:simons_pub_uploader/upload.dart';
 import 'package:tools/sass_package.dart';
 
-const _version = '5.1.3';
+const _version = '5.2.2';
 const _fs = LocalFileSystem();
 
 Future<void> main() async {
